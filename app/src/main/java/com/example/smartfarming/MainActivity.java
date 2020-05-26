b8996c5041db4d8b52387c8aab3b543718408d58
Vincenzo De Martino <52664512+kenz097@users.noreply.github.com>
2020-05-26 13:42:55 +0100

    iniziata la pagina di login, aggiunti i colori per i bottoni, tabelle e nomi pagine.

--- app/src/main/java/com/example/smartfarming/MainActivity.java
+++ app/src/main/java/com/example/smartfarming/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.smartfarming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button accedi;
+    private EditText email;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        accedi=findViewById(R.id.accedi);
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.password);
     }
 }
