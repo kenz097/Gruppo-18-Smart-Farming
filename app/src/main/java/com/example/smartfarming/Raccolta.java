73022fecb608b4d020aef4ec9fa5a8539eeb8b19
MMeMM <MeM@MBP-di-Marco.lan>
2020-05-26 19:21:57 +0200

    Added Raccolta Interface

---
+++ app/src/main/java/com/example/smartfarming/Raccolta.java
@@ -0,0 +1,13 @@
+package com.example.smartfarming;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Raccolta extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raccolta);
+    }
+}
