ac2f1a25e638f84ddc3dc4558098c994b0e6f183
MMeMM <MeM@MBP-di-Marco.lan>
2020-05-26 17:49:54 +0200

    Added Hompage class with relative xml file for Vincenzo

--- app/src/main/java/com/example/smartfarming/Stoccaggio.java
+++ app/src/main/java/com/example/smartfarming/Stoccaggio.java
@@ -1,6 +1,9 @@
 package com.example.smartfarming;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import java.util.ArrayList;
 
 public class Stoccaggio extends AppCompatActivity {
     private ListView list,list2;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,7 @@ public class Stoccaggio extends AppCompatActivity {
 
        list=(ListView)findViewById(R.id.list);
        list2=(ListView)findViewById(R.id.list2);
+       button=(Button)findViewById(R.id.back);
 
         ArrayList<Prodotti_Stoccaggio_Table1> array= new ArrayList<>();
         ArrayList<Prodotti_Stoccaggio_Table2> array2= new ArrayList<>();
@@ -35,5 +40,9 @@ public class Stoccaggio extends AppCompatActivity {
         CustomAdapterStoccaggio2 custom2= new CustomAdapterStoccaggio2(this, array2);
         list.setAdapter(custom1);
         list2.setAdapter(custom2);
+    }
+    public void goBack(View view){
+        Intent intent= new Intent(this, Homepage.class);
+        startActivity(intent);
     }
 }
