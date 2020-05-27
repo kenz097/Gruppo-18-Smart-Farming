06824a7473e11f712b958c5a6b106ce240ca870e
Vincenzo De Martino <52664512+kenz097@users.noreply.github.com>
2020-05-27 11:42:54 +0100

    Corretta la pagina di login e creata homepage, ci sono dei problemi però sul cambiare inferfaccia e sull'alert dialog

--- app/src/main/java/com/example/smartfarming/Homepage.java
+++ app/src/main/java/com/example/smartfarming/Homepage.java
@@ -10,4 +10,6 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
     }
+
+
 }
--- app/src/main/java/com/example/smartfarming/MainActivity.java
+++ app/src/main/java/com/example/smartfarming/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.smartfarming;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,12 +13,36 @@ public class MainActivity extends AppCompatActivity {
     private Button accedi;
     private EditText email;
     private EditText password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        accedi=findViewById(R.id.accedi);
-        email=findViewById(R.id.email);
-        password=findViewById(R.id.password);
+        accedi = findViewById(R.id.accedi);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
+    }
+
+
+    public void checkLogin() {
+        if (email.toString().equals("giacomomancini@gmail.com")) {
+            if (password.toString().equals("ciaociao")) {
+                Intent intent = new Intent(this, Homepage.class);
+                startActivity(intent);
+            }
+        } else {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Errore");
+            alert.setMessage("Email o password errati");
+            alert.setPositiveButton("Capito", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    //set what would happen when positive button is clicked
+                    finish();
+                }
+            });
+            alert.show();
+        }
     }
 }
