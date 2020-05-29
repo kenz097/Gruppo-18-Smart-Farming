c4a3f59b805d0bfc8f92b56a2d1467d64165ebbb
marcociano <m.ciano4@studenti.unisa.it>
2020-05-29 16:27:50 +0200

    Added Irrigazione interface

--- app/src/main/java/com/example/smartfarming/Irrigazione.java
+++ app/src/main/java/com/example/smartfarming/Irrigazione.java
@@ -2,11 +2,38 @@ package com.example.smartfarming;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-public class Irrigazione extends AppCompatActivity {
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Irrigazione extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_irrigazione);
+
+        SupportMapFragment mapFragment=(SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+
+        LatLng campi= new LatLng(40.780981, 14.792098);
+        mMap.addMarker(new MarkerOptions().position(campi).title("Coltiviamo!"));
+        CameraPosition cameraPosition= new CameraPosition.Builder().target(campi).zoom(30).build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
     }
 }
