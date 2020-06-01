b96df696478c774dbfdfcd9668945c079dba1a4b
Vincenzo De Martino <52664512+kenz097@users.noreply.github.com>
2020-06-01 15:58:09 +0100

    bug fix e finita raccolta
    Aggiustato l'alertDialog e ora procedo a farlo funzionare anche nella login e nel solco

--- app/src/main/java/com/example/smartfarming/Raccolta.java
+++ app/src/main/java/com/example/smartfarming/Raccolta.java
@@ -4,11 +4,14 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,43 +31,49 @@ public class Raccolta extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_raccolta);
-        nome_prodotto=(EditText)findViewById(R.id.nome_prodotto);
-        posizione=(EditText)findViewById(R.id.posizione);
-        quantita=(EditText)findViewById(R.id.quantita);
-        numero_prodotti=(EditText)findViewById(R.id.numero_prodotti);
-        list=(ListView)findViewById(R.id.list);
+        nome_prodotto = (EditText) findViewById(R.id.nome_prodotto);
+        posizione = (EditText) findViewById(R.id.posizione);
+        quantita = (EditText) findViewById(R.id.quantita);
+        numero_prodotti = (EditText) findViewById(R.id.numero_prodotti);
+        list = (ListView) findViewById(R.id.list);
 
     }
-    public void goBack(View view){
-        Intent intent= new Intent(this, Homepage.class);
+
+    public void goBack(View view) {
+        Intent intent = new Intent(this, Homepage.class);
         startActivity(intent);
     }
 
-    public void addProduct(View v){
+    public void addProduct(View v) {
 
-        el1=nome_prodotto.getText().toString();
-        el2=posizione.getText().toString();
-        el3=quantita.getText().toString();
-        el4=numero_prodotti.getText().toString();
+        el1 = nome_prodotto.getText().toString();
+        el2 = posizione.getText().toString();
+        el3 = quantita.getText().toString();
+        el4 = numero_prodotti.getText().toString();
 
         if (el1.equalsIgnoreCase("")) {
             Toast.makeText(Raccolta.this, "Hai dimenticato il nome del prodotto", Toast.LENGTH_LONG).show();
-        } else if(el2.equalsIgnoreCase("")){
+        } else if (el2.equalsIgnoreCase("")) {
             Toast.makeText(Raccolta.this, "Hai dimenticato la posizione del prodotto", Toast.LENGTH_LONG).show();
-        }else if(el3.equalsIgnoreCase("")){
+        } else if (el3.equalsIgnoreCase("")) {
             Toast.makeText(Raccolta.this, "Hai dimenticato la quantita' del prodotto", Toast.LENGTH_LONG).show();
-        }else if(el4.equalsIgnoreCase("")){
+        } else if (el4.equalsIgnoreCase("")) {
             Toast.makeText(Raccolta.this, "Hai dimenticato il numero del prodotto", Toast.LENGTH_LONG).show();
-        }else {
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(Raccolta.this);
+            final View views = LayoutInflater.from(Raccolta.this).inflate(R.layout.form_done, null);
+            TextView title = views.findViewById(R.id.title_add);
+            TextView body = views.findViewById(R.id.full_text);
+            title.setText("Inserimento quantità nel sistema");
+            body.setText("Quantita' inserita correttamente\n              nel sistema");
+            builder.setView(views);
+            builder.show();
+
             ArrayList<Quantita_Raccolta_Table> arrayList = new ArrayList<>();
             arrayList.add(new Quantita_Raccolta_Table(el1, el2, el3, el4));
             CustomAdapterRaccolta custom = new CustomAdapterRaccolta(this, arrayList);
             list.setAdapter(custom);
 
-            AlertDialog.Builder alert = new AlertDialog.Builder(this);
-            alert.setTitle("Inserimento quantità nel sistema");
-            alert.setMessage("Quantità inserita correttamente nel sistema");
-            alert.show();
         }
     }
 }
